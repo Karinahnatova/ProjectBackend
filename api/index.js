c6b552b7d6a3c63805c9e0949d2cdaa32a76ba1d
karinahnatova <karinahnatova.catorce@alumnos.cei.es>
2024-05-04 23:44:39 +0200

    building

--- api/index.js
+++ api/index.js
@@ -6,31 +6,23 @@ import connectDB from "./database/database.js";
 
 const app = express();
 
-// Best to avoid console.clear in production code to maintain log integrity.
 // console.clear();
 
 // connectDB();
+app.use("/html-form", express.static("public"));
+app.use("/files", express.static("uploads"));
 
-// Setup for serving static HTML form and uploaded files
-// Ensure these directories are appropriately configured in your Vercel project
-app.use("/html-form", express.static("public")); // Static HTML forms
-app.use("/files", express.static("uploads")); // Folder for uploaded files (consider using cloud storage in production)
+app.use(cors());
+app.use(logger);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use(cors()); // Apply CORS
-app.use(logger); // Apply custom logger middleware
-app.use(express.json()); // For parsing application/json
-app.use(express.urlencoded({ extended: true })); // For parsing application/x-www-form-urlencoded
-
-// Main API routes
 app.use("/api/v1/", router);
 
-// Root endpoint to serve HTML directly (consider serving through static files)
 app.get("/", (req, res) => {
   res.setHeader("Content-Type", "text/html");
   const landingHTML = `<h1>Welcome to Our HTML API</h1>`;
   res.send(landingHTML);
 });
 
-// Since you're deploying to Vercel, listening locally is not needed here
-// The app will be exported and Vercel will handle the invocation
 export default app;
